8f4966ad8accd6e2c2ddacc5b43a443dbe2f0404
Mr-peipei <wushumenmenmen2@gmail.com>
2022-11-16 23:09:18 +0900

    refactor function name

--- src/math/array_sum.rs
+++ src/math/array_sum.rs
@@ -1,4 +1,4 @@
-pub fn simple_array_sum(ar: &[i32]) -> i32 {
+pub fn array_sum(ar: &[i32]) -> i32 {
     let sum: i32 = ar.iter().sum();
     sum
 }
@@ -10,7 +10,7 @@ mod tests {
 
     #[test]
     fn basic_big_sum() {
-        assert_eq!(simple_array_sum(&[2, 2]), 4);
-        assert_eq!(simple_array_sum(&[1, 2, 3]), 6);
+        assert_eq!(array_sum(&[2, 2]), 4);
+        assert_eq!(array_sum(&[1, 2, 3]), 6);
     }
 }
--- src/math/big_array_sum.rs
+++ src/math/big_array_sum.rs
@@ -1,4 +1,4 @@
-pub fn big_sum(ar: &[i64]) -> i64 {
+pub fn big_array_sum(ar: &[i64]) -> i64 {
     let sum: i64 = ar.iter().sum();
     sum
 }
@@ -10,7 +10,7 @@ mod tests {
 
     #[test]
     fn basic_big_sum() {
-        assert_eq!(big_sum(&[2, 2]), 4);
-        assert_eq!(big_sum(&[1, 2, 3]), 6);
+        assert_eq!(big_array_sum(&[2, 2]), 4);
+        assert_eq!(big_array_sum(&[1, 2, 3]), 6);
     }
 }
